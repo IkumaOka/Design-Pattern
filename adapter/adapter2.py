049e71de9b952935e07db043e1791144d41016d8
Ikuma Oka <ikuma1018soccer06@gmail.com>
2022-08-27 00:10:09 +0900

    adapterの委譲を実装

---
+++ adapter/adapter2.py
@@ -0,0 +1,40 @@
+from abc import ABCMeta, abstractmethod
+
+# 委譲を使ったパターン(p.20~)
+class Banner:
+    def __init__(self, string):
+        self.string = string
+    
+    def show_with_paren(self):
+        print(f'({self.string})')
+    
+    def show_with_aster(self):
+        print(f'*{self.string}*')
+
+
+class Print(metaclass=ABCMeta):
+    @abstractmethod
+    def print_weak():
+        pass
+
+    @abstractmethod
+    def print_strong():
+        pass
+
+
+class PrintBanner(Print):
+    def __init__(self, string):
+        # Bannerインスタンスを作成し、print_weak()とprint_strong()でBannerクラスに処理を任せている
+        self.banner = Banner(string)
+
+    def print_weak(self):
+        self.banner.show_with_paren()
+
+    def print_strong(self):
+        self.banner.show_with_aster()
+
+
+if __name__ == '__main__':
+    p = PrintBanner('Hello')
+    p.print_weak()
+    p.print_strong()
